3d4f5e9611c6a1a793542eba4fb87a89b600cb39
Markus Schmall <schmall.markus@googlemail.com>
2012-01-28 08:41:24 +0100

    added correct names import codes for mysql

---
+++ importmsql.sh
@@ -0,0 +1,3 @@
+java -classpath ./servletpot.jar:/opt/servletpot/web/WEB-INF/lib/mysql.jar org.metams.ServletPot.tools.ImporterMysql servletpot pw100pw200 jdbc:mysql://localhost/ServletPot /opt/servletpot/web/WEB-INF/strings.txt
+
+
