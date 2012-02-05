c5ad7b5eed9e55470b5767bcaada37e4e2583b54
Markus Schmall <schmall.markus@googlemail.com>
2012-02-05 21:05:04 +0100

    redis handling added

---
+++ src/org/metams/ServletPot/tools/CheckRedis.java
@@ -0,0 +1,12 @@
+package org.metams.ServletPot.tools;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: flake
+ * Date: 1/24/12
+ * Time: 10:07 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CheckRedis
+{
+}
