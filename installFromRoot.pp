9e00165524aaa4689de152ba800c885a9b8e59cd
Markus Schmall <schmall.markus@gmail.com>
2013-01-24 21:35:08 +0100

    added basic package installation

--- installFromRoot.pp
+++ installFromRoot.pp
@@ -22,6 +22,14 @@ package { 'mono-complete' :
 	  ensure => present,
 }
 
+package { 'ant' :
+	  ensure => present,
+}
+
+package { 'nant' :
+	  ensure => present,
+}
+
 package { 'git' :
 		ensure => present,
 }
@@ -61,26 +69,34 @@ user { 'honeypot':
   	uid     => '1001',
 }
 
+file { "/home/honeypot/":
+    ensure => "directory",
+    owner  => "honeypot",
+    mode   => 777,
+    before => File['/home/honeypot/tools'],
+}
 
+file { "/home/honeypot/tools":
+    ensure => "directory",
+    owner  => "honeypot",
+    mode   => 777,
+}
 
 
 $password = "very_hard_pw"
 
-  exec { "Set MySQL server root password":
-#    subscribe => [ Package["MySQL-server"], Package["MySQL-client"], Package["MySQL-shared"] ],
+exec { "Set MySQL server root password":
     refreshonly => true,
     unless => "mysqladmin -uroot -p$password status",
     path => "/bin:/usr/bin",
     command => "mysqladmin -uroot password $password",
-  }
+}
 
 
-# svn checkout http://kippo.googlecode.com/svn/trunk/ kippo-read-only
+
+Package['openjdk-7-jdk'] -> Package['mysql-server'] -> Exec['Set MySQL server root password'] -> User['honeypot'] -> File["/home/honeypot/tools"] -> Package['tomcat7'] -> Package['mono-complete'] -> Package['git'] -> Package['authbind'] ->Package['python-twisted'] -> Package['nano'] -> Package['joe'] -> Package['subversion'] -> Package['ant'] -> Package['nant']
 
 
-Package['openjdk-7-jdk'] -> Package['mysql-server'] -> Exec['Set MySQL server root password'] -> User['honeypot']
-Package['tomcat7'] -> Package['mono-complete'] -> Package['git'] -> Package['authbind'] -> Package['subversion']
-Package['python-twisted'] -> Package['nano'] -> Package['joe']
 
 
 
